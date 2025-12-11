831fec526ba1a06bb79173a7a88132fe21cc3332
Larissa Cox (SHE|HER) <laco@microsoft.com>
2025-12-11 15:18:21 -0800

    removing all but linked scripts children

--- .devcenter/catalog/image-definitions/linked-script-parent/nest1/nest2/write-to-file-user.ps1
+++
@@ -1,1 +0,0 @@
-echo "This is from a cached image definition file in a user task that is a few folders down!!" >> C:\\imaging-cached-script.txt
--- .devcenter/catalog/image-definitions/linked-script-parent/nest1/nest2/write-to-file.ps1
+++
@@ -1,1 +0,0 @@
-echo "This is from a cached image definition file that is a few folders down!!" >> C:\\imaging-cached-nested.txt
