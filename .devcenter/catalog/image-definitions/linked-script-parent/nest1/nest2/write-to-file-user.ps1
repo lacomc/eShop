975a8924b6827295eec71ff9ec9b63f1e7dfb452
Larissa Cox (SHE|HER) <laco@microsoft.com>
2025-12-15 16:28:47 -0800

    updating user tasks to write to %TEMP%

--- .devcenter/catalog/image-definitions/linked-script-parent/nest1/nest2/write-to-file-user.ps1
+++ .devcenter/catalog/image-definitions/linked-script-parent/nest1/nest2/write-to-file-user.ps1
@@ -1,1 +1,1 @@
-echo "This is from a cached image definition file in a user task that is a few folders down!!" >> C:\\Users\\LarissaCox\\Documents\\linked-script-parent-user-nested.txt
+echo "This is from a cached image definition file in a user task that is a few folders down!!" >> $env:TEMP\\linked-script-parent-user-nested.txt
--- .devcenter/catalog/image-definitions/linked-script-parent/write-to-file.ps1
+++ .devcenter/catalog/image-definitions/linked-script-parent/write-to-file.ps1
@@ -1,1 +1,1 @@
-echo "This is from a cached image definition file!!" >> C:\\linked-script-parent1.txt
+echo "This is from a cached image definition file #1!!" >> C:\\linked-script-parent1.txt
